16123b84346475b8072906bc9287d2d5ad6bdc1d
Pedro Gustavo <37873313+PedroGustavoFM@users.noreply.github.com>
2020-06-06 12:34:18 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="css/index.css">
+
+    <title>Sistema Patrimônio</title>
+</head>
+<body>
+    <div class="content">
+        <h1>WEB Patrio</h1>
+
+        <form action="" method="post">
+            <input type="text" name="name_user" id="name_user" placeholder="Username" class="input_geral">
+            </br>
+            <input type="password" name="pass_user" id="pass_user" placeholder="Password" class="input_geral">
+            </br>
+            <input type="submit" value="Login" class="button_class button_login">
+            <input type="reset" value="Limpar" class="button_class button_reset">
+        </form>
+    </div>
+</body>
+</html>
